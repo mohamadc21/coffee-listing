f544ae1860d06843b757e2aa721c9b01027d1d53
M21 <wyattmohammad1371017@gmail.com>
2024-02-23 17:50:57 +0330

    change banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 function Banner() {
   return (
-    <div className="bg-[url('bg-cafe.jpg')] bg-cover bg-center bg-no-repeat h-[250px] w-full"></div>
+    <div className="bg-[url('assets/images/bg-cafe.jpg')] bg-cover bg-center bg-no-repeat h-[250px] w-full"></div>
   )
 }
 
